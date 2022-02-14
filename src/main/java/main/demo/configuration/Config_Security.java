f42ee4ae87085f551c0ed00226a924d35c6d7353
Seokwon-kkong <ksw@rowan.kr>
2022-02-14 18:09:14 +0900

    configuration 설정 구현 중

---
+++ src/main/java/main/demo/configuration/Config_Security.java
@@ -0,0 +1,32 @@
+package main.demo.configuration;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableWebSecurity // 웹 보안 활성화를 위한 annotation => SpringSecurityFilterChain에 등록됨.
+public class Config_Security extends WebSecurityConfigurerAdapter {
+
+    // HttpSecurity
+    // A HttpSecurity is similar to Spring Security's XML <http> element in the namespace configuration.
+    // It allows configuring web based security for specific http requests
+    // By default it will be applied to all requests, but can be restricted using requestMatcher(RequestMatcher) or other similar methods.
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+
+    // WebSecurity
+    // Customizations to the WebSecurity can be made by creating a WebSecurityConfigurer,
+    // overriding WebSecurityConfigurerAdapter or exposing a WebSecurityCustomizer bean.
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+}
